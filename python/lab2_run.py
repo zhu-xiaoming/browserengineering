c6ff6ba0fd65812a8c174a9aef532d84e5c7c8ee
zhuxiaoming <zhuxm812@outlook.com>
2022-08-30 18:37:57 +0800

    fix: update test url

--- python/lab2_run.py
+++ python/lab2_run.py
@@ -3,4 +3,4 @@ from lab2 import Browser
 if __name__ == "__main__":
     import sys
 
-    Browser().load('https://www.zggdwx.com/xiyou/1.html')
+    Browser().load('https://browser.engineering/examples/xiyouji.html')
